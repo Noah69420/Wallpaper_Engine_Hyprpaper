79163bfb5aa163930cee22d3a52b8eac7b7cb737
Noah69420 <noah.balsiger@gmail.com>
2024-12-05 12:23:28 +0100

    formatting

--- wallpaper.py
+++ wallpaper.py
@@ -19,10 +19,11 @@ timer_old: float = 0
 prozess = ""
 run: bool = True
 subprocess_alive: bool = False
-wallpaper_change: bool = False
+allow_wallpaper_change: bool = False
 
 # Modus
 freeze: bool = False
+
 
 def push_notification(msg):
     if notification:
@@ -50,10 +51,12 @@ def update_hyprpaper_config(wallpaper_path):
 
 def restart_hyprpaper(timer: float):
     global subprocess_alive, timer_old, prozess
+    
     if timer >= timer_old + sleep_time and subprocess_alive or not run:
         prozess.kill()
         prozess.wait()
         subprocess_alive = False
+
     if not subprocess_alive and run:
         prozess = subprocess.Popen(["hyprpaper"])
         subprocess_alive = True
@@ -63,27 +66,26 @@ def restart_hyprpaper(timer: float):
 def zufall(wallpapers):
     bild = random.choices(wallpapers)[0]
     return bild
-        
-
 
 
 def read_pipe():
-    global run, timer_old, freeze, sleep_time, wallpaper_change
+    global run, timer_old, freeze, sleep_time, allow_wallpaper_change
     while run:
         with open(FIFO, "r",  encoding="utf-8") as fifo:
             for line in fifo:
                 line = line.removesuffix("\n")
                 if line == "kill":
                     freeze = False
-                    wallpaper_change = True
+                    allow_wallpaper_change = True
                     run = False
                     push_notification("kill Wallpaper_Engine_Hyprpaper and Hyprpaper :(")
                     print("Stop thread")
                     break
+
                 if "next" == line:
                     timer_old = timer_old - sleep_time
                     push_notification("skip wallpaper")
-                    wallpaper_change = True
+                    allow_wallpaper_change = True
 
                 if "freeze" == line:
                     if freeze:
@@ -100,10 +102,12 @@ def read_pipe():
                     except ValueError:
                         print("Value not correct")
 
+
 def change_wallpaper(bild_alt, bild_neu, timer):
     global timer_old
     wallpapers = os.listdir(path_backgrounds)
     print(f"\nbild_alt: {bild_alt}\n")
+
     while bild_alt == bild_neu:
         bild_neu = zufall(wallpapers)
         print(f"bild_neu: {bild_neu}\n")
@@ -112,6 +116,7 @@ def change_wallpaper(bild_alt, bild_neu, timer):
     update_hyprpaper_config(bild_neu)
     restart_hyprpaper(timer)
     return bild_alt, bild_neu
+
 
 def main():
     try:
@@ -123,8 +128,8 @@ def main():
     wallpapers = os.listdir(path_backgrounds)
     bild_neu = zufall(wallpapers)
     bild_alt, bild_neu = change_wallpaper(bild_alt, bild_neu, timer)
-
-    global run, wallpaper_change
+    
+    global run, allow_wallpaper_change
     try:
         threading.Thread(target=read_pipe).start()
         while run:
@@ -132,13 +137,13 @@ def main():
             print(f"time_now: {timer}")
             print(f"OLD_Timer: {timer_old}")
             if timer >= timer_old + sleep_time:
-                wallpaper_change = True
+                allow_wallpaper_change = True
             time.sleep(1)
             if freeze:
                 continue
-            elif wallpaper_change:
+            elif allow_wallpaper_change:
                 bild_alt, bild_neu = change_wallpaper(bild_alt, bild_neu, timer)
-                wallpaper_change = False
+                allow_wallpaper_change = False
         os.remove(FIFO)
     except KeyboardInterrupt:
         run = False
